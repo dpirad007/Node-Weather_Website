c2189c36161be5764c0b2e4a755af38acd040be8
Dion Pinto <dionpinto000@gmail.com>
2020-03-25 16:28:03 +0530

    remove unneccesary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log("client side javascript file")
-
-
-
 const weatherform = document.querySelector('form')
 const search =document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
